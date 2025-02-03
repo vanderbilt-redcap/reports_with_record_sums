9bfac6c432f2550143e01ab09e7d8934e49bd96d
moorejr5 <james.r.moore@vumc.org>
2025-02-03 08:24:29 -0600

    Removing call to 'LoadJSBS' function in report because including the header/footer from REDCap already includes those files. Redundant bootstrap calls were causing formatting issues.

--- interfaces/data_report.php
+++ interfaces/data_report.php
@@ -1,5 +1,4 @@
 <?php
-require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
 
 $project_id = $_GET['pid'];
 $report_index = (is_numeric($_POST['report_index']) ? $_POST['report_index'] : null);
@@ -8,4 +7,3 @@ if (is_numeric($project_id)) {
 	$module->loadTwigExtensions();
 	echo $module->loadDataReportTwig($project_id, $report_index);
 }
-require_once APP_PATH_DOCROOT . 'ProjectGeneral/footer.php';
